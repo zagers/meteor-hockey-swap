f5077afe5c1112baeea96c15aa6f243d0f20ebb4
Scott Zager <scott.zager@gmail.com>
2014-12-19 14:52:08 +0000

    Updated fixtures to set userId & dateAdded correctly

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,29 @@
 if (Available.find().count() === 0){
-	Available.insert({
+  
+  var joeId = Meteor.users.insert({
+    username: 'joe'
+  })
+  
+  Accounts.setPassword(joeId, 'abc123') 
+	
+  Available.insert({
 		itemName: 'Skates',
-		itemDescription: 'Bauer adult size 7',	
+		itemDescription: 'Bauer adult size 7',
+    userId: joeId,
+    dateAdded: new Date()
 	});
 	
 	Available.insert({
 		itemName: 'Helmet',
-		itemDescription: 'Mission youth large'	
+		itemDescription: 'Mission youth large',
+    userId: joeId,
+    dateAdded: new Date()
 	});
 	
 	Available.insert({
 		itemName: 'Pants',
-		itemDescription: 'Adult large'
+		itemDescription: 'Adult large',
+    userId: joeId,
+    dateAdded: new Date()
 	});
 }
