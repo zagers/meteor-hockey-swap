426036cdf3f3602dda6aa68cc8b351e7601b56e3
Scott Zager <scott.zager@gmail.com>
2014-12-23 21:02:26 +0000

    Added user name to item (modified login to require username)

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -2,3 +2,6 @@ UI.registerHelper("prettifyDate", function(date) {
     return new moment(date).format('DD-MMM-YYYY')
 });
 
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_EMAIL'
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -5,25 +5,38 @@ if (Items.find().count() === 0){
   })
   
   Accounts.setPassword(joeId, 'abc123') 
+
+  for (i = 0; i <= 50; i++){
+	Items.insert({
+		itemName: Fake.sentence(3),
+		itemDescription: Fake.sentence(7),
+		userId: joeId,
+		userName: 'joe',
+		dateAdded: new Date()
+	});
+  }
 	
   Items.insert({
 		itemName: 'Skates',
 		itemDescription: 'Bauer adult size 7',
-    userId: joeId,
-    dateAdded: new Date()
+    	userId: joeId,
+		userName: 'joe',
+    	dateAdded: new Date()
 	});
 	
 	Items.insert({
 		itemName: 'Helmet',
 		itemDescription: 'Mission youth large',
-    userId: joeId,
-    dateAdded: new Date()
+    	userId: joeId,
+		userName: 'joe',
+    	dateAdded: new Date()
 	});
 	
 	Items.insert({
 		itemName: 'Pants',
 		itemDescription: 'Adult large',
-    userId: joeId,
-    dateAdded: new Date()
+		userName: 'joe',
+    	userId: joeId,
+    	dateAdded: new Date()
 	});
 }
